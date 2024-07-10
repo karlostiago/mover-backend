d2cfd788afbfdfc92417535930fb3d48810088a5
FireXtz <phantomrising@protonmail.com>
2024-07-10 20:00:26 -0300

    add config file for email bean configuration, enable cors filter and refactory method existCpfRegistered and change port configuration 8081 t0 8082 evicted conflicts for front end.

--- src/main/java/com/ctsousa/mover/core/exception/handler/NotificationHandlerException.java
+++ src/main/java/com/ctsousa/mover/core/exception/handler/NotificationHandlerException.java
@@ -3,6 +3,7 @@ package com.ctsousa.mover.core.exception.handler;
 import com.ctsousa.mover.core.exception.builder.ErrorBuilder;
 import com.ctsousa.mover.core.exception.notification.NotificationException;
 import com.ctsousa.mover.core.exception.error.Error;
+import com.ctsousa.mover.core.exception.notification.NotificationNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,16 @@ public class NotificationHandlerException extends ResponseEntityExceptionHandler
     public ResponseEntity<List<Error>> handleNotificationException(NotificationException ex) {
         List<Error> errors = List.of((ErrorBuilder.builder()
                 .withStatus(HttpStatus.BAD_REQUEST)
+                .withMessage(ex.getMessage())
+                .withSeverity(ex.getSeverity())
+                .build()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler({NotificationNotFoundException.class})
+    public ResponseEntity<List<Error>> handleNotificationNotFoundException(NotificationNotFoundException ex) {
+        List<Error> errors = List.of((ErrorBuilder.builder()
+                .withStatus(HttpStatus.NOT_FOUND)
                 .withMessage(ex.getMessage())
                 .withSeverity(ex.getSeverity())
                 .build()));
--- src/main/java/com/ctsousa/mover/service/impl/ClientServiceImpl.java
+++ src/main/java/com/ctsousa/mover/service/impl/ClientServiceImpl.java
@@ -2,6 +2,7 @@ package com.ctsousa.mover.service.impl;
 
 import com.ctsousa.mover.core.entity.ClientEntity;
 import com.ctsousa.mover.core.exception.notification.NotificationException;
+import com.ctsousa.mover.core.exception.notification.NotificationNotFoundException;
 import com.ctsousa.mover.core.service.impl.AbstractServiceImpl;
 import com.ctsousa.mover.core.validation.CpfValidator;
 import com.ctsousa.mover.repository.ClientRepository;
@@ -23,18 +24,13 @@ public class ClientServiceImpl extends AbstractServiceImpl <ClientEntity, Long> 
 
     @Override
     public ClientEntity existsCpfRegistered(String cpf) {
-
-        if (StringUtils.isBlank(cpf)) {
-            throw new NotificationException("CPF não fornecido corretamente");
-        }
-
-        if (!CpfValidator.isValid(cpf)) {
-            throw new NotificationException("CPF inválido");
+        if (StringUtils.isBlank(cpf) || !CpfValidator.isValid(cpf)) {
+            throw new NotificationException("CPF inválido ou não fornecido corretamente");
         }
 
         ClientEntity client = clientRepository.existsCpfRegisteredInApplication(cpf);
         if (client == null) {
-            throw new NotificationException("CPF não encontrado no sistema");
+            throw new NotificationNotFoundException("CPF não encontrado no sistema");
         }
 
         return client;
