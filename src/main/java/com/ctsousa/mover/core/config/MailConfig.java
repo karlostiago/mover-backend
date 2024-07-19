05d2aa9588c7bafe738ffd4c9a57526a07fad346
FireXtz <phantomrising@protonmail.com>
2024-07-19 10:48:34 -0300

    feat: mail config enable enviororment values for mail credentials.

--- src/main/java/com/ctsousa/mover/core/config/MailConfig.java
+++ src/main/java/com/ctsousa/mover/core/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.ctsousa.mover.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,22 +11,46 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private int mailPort;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Value("${spring.mail.properties.mail.smtp.ssl.trust}")
+    private String mailSmtpSslTrust;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean mailSmtpAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean mailSmtpStarttlsEnable;
+
+    @Value("${spring.mail.properties.mail.smtp.socketFactory.class}")
+    private String mailSmtpSocketFactoryClass;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(465);
+        mailSender.setHost(mailHost);
+        mailSender.setPort(mailPort);
 
-        mailSender.setUsername("dev@gmail.com");
-        mailSender.setPassword("password");
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.auth", mailSmtpAuth);
         props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.ssl.trust", "*");
-        props.put("mail.smtp.starttls.enable", "false");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.ssl.trust", mailSmtpSslTrust);
+        props.put("mail.smtp.starttls.enable", mailSmtpStarttlsEnable);
+        props.put("mail.smtp.socketFactory.class", mailSmtpSocketFactoryClass);
         props.put("mail.debug", "true");
 
         return mailSender;
