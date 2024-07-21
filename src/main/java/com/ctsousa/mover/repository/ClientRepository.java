670d0f558a211aee402c991cd434fabd3b1f70ce
FireXtz <phantomrising@protonmail.com>
2024-07-21 17:13:30 -0300

    fix: unussed name incorret postgres to mysql and refact function search cpf

--- src/main/java/com/ctsousa/mover/repository/ClientRepository.java
+++ src/main/java/com/ctsousa/mover/repository/ClientRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
 
-    @Query(value = "SELECT c FROM ClientEntity c WHERE translate(c.cpf, '.-', '') = translate(?1, '.-', '')")
+    @Query(value = "SELECT c FROM ClientEntity c WHERE REPLACE(REPLACE(REPLACE(c.cpf, '.', ''), '-', ''), ' ', '') = REPLACE(REPLACE(REPLACE(?1, '.', ''), '-', ''), ' ', '')")
     ClientEntity existsCpfRegisteredInApplication(String cpf);
+
 }
