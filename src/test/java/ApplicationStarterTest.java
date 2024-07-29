dcfc52ebdd135a81791b71e74af861e654a88173
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-07-29 17:16:32 -0300

    feat: implementacao do teste de start da aplicacao

---
+++ src/test/java/ApplicationStarterTest.java
@@ -0,0 +1,20 @@
+import com.ctsousa.mover.ApplicationStarter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest (classes = ApplicationStarter.class)
+public class ApplicationStarterTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void shouldStarterApplication() {
+        Assertions.assertNotNull(context);
+    }
+}
