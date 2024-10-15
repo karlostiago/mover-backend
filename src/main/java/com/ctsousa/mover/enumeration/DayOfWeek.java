a7437b4561b50b61aa6ea2d3d93f2f9b0acbfcbc
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-10-15 18:00:31 -0300

    refact: ajustando enums frequencia pagamento e dia da semana

--- src/main/java/com/ctsousa/mover/enumeration/DayOfWeek.java
+++ src/main/java/com/ctsousa/mover/enumeration/DayOfWeek.java
@@ -1,11 +1,38 @@
 package com.ctsousa.mover.enumeration;
 
+import com.ctsousa.mover.core.exception.notification.NotificationException;
+
+import static com.ctsousa.mover.core.util.StringUtil.toUppercase;
+
 public enum DayOfWeek {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
+    SUNDAY(1, "DOMINGO"),
+    MONDAY(2, "SEGUNDA-FEIRA"),
+    TUESDAY(3, "TERÇA-FEIRA"),
+    WEDNESDAY(4, "QUARTA-FEIRA"),
+    THURSDAY(5, "QUINTA-FEIRA"),
+    FRIDAY(6, "SEXTA-FEIRA"),
+    SATURDAY(7, "SÁBADO");
+
+    private final Integer code;
+    private final String description;
+
+    DayOfWeek(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static DayOfWeek toDescription(String description) {
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            if (dayOfWeek.description.equals(description)
+                    || toUppercase(dayOfWeek.name()).equals(toUppercase(description))) return dayOfWeek;
+        }
+        throw new NotificationException("Dia da semana não suportada :: " + description);
+    }
+
+    public static DayOfWeek toCode(Integer code) {
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            if (dayOfWeek.code.equals(code)) return dayOfWeek;
+        }
+        throw new NotificationException("Código dia da semana não suportada :: " + code);
+    }
 }
--- src/main/java/com/ctsousa/mover/enumeration/PaymentFrequency.java
+++ src/main/java/com/ctsousa/mover/enumeration/PaymentFrequency.java
@@ -1,7 +1,34 @@
 package com.ctsousa.mover.enumeration;
 
+import com.ctsousa.mover.core.exception.notification.NotificationException;
+
+import static com.ctsousa.mover.core.util.StringUtil.toUppercase;
+
 public enum PaymentFrequency {
-    WEEKLY,
-    BIWEEKLY,
-    MONTHLY
+    WEEKLY(1, "SEMANAL"),
+    BIWEEKLY(2, "QUINZENAL"),
+    MONTHLY(3, "MENSAL");
+
+    private final Integer code;
+    private final String description;
+
+    PaymentFrequency(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static PaymentFrequency toDescription(String description) {
+        for (PaymentFrequency paymentFrequency : PaymentFrequency.values()) {
+            if (paymentFrequency.description.equals(description)
+                    || toUppercase(paymentFrequency.name()).equals(toUppercase(description))) return paymentFrequency;
+        }
+        throw new NotificationException("Frequência de pagamento não suportada :: " + description);
+    }
+
+    public static PaymentFrequency toCode(Integer code) {
+        for (PaymentFrequency paymentFrequency : PaymentFrequency.values()) {
+            if (paymentFrequency.code.equals(code)) return paymentFrequency;
+        }
+        throw new NotificationException("Código frequência de pagamento não suportada :: " + code);
+    }
 }
